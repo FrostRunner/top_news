2e38f23cb455f0b3d379fbcdc6f9839c53eac471
Sergey Fomin <PivoWar89@gmail.com>
2022-09-06 16:30:23 +0300

    add fast api - file restructure

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-response = requests.get("https://lenta.ru/")
-response.raise_for_status()
-
-content = response.content
-
-soup = BeautifulSoup(content, 'html.parser')
-print(soup.title.text)
-
-
-for item in list(map(lambda x: (x.text[:-5], x.text[-5:], x["href"]), soup.find_all("a", {"class": "_topnews"}))):
-    print(item)
-
-
-if __name__ == "__main__":
-    print("qwe")
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
